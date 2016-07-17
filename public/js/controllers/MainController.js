b7ca968fa8a16f80245008e891d959c0536f8e99
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-07-17 17:09:46 -0400

    updated the 1 week work and added services folder in js

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -3,9 +3,51 @@
   angular.module('ngtodos') //getter
          .controller('MainController', MainController);
 
-  MainController.$inject = [];
+  MainController.$inject = ['$scope', 'TodoService'];
 
-  function MainController(){
-    console.log('Main!');
+  function MainController($scope, TodoService){
+  $scope.todos = TodoService.todos;
+  $scope.create = createTodo;
+  $scope.delete = deleteTodo;
+  $scope.edit = editTodo;
+  $scope.update = updateTodo;
+  getTodos();
+
+  function editTodo(todo){
+    todo.editing = true;
+  }
+  function updateTodo(todo){
+    todo.editing = false;
+    todo.isComplete = todo.isComplete.toString();
+    TodoService.update(todo.id, todo)
+              .then(function(){
+                getTodos();
+              });
+  }
+
+  function getTodos(){
+    TodoService.readAll()
+              .then(function(){
+                $scope.todos = TodoService.todos;
+                console.log($scope.todos);
+              });
+  }
+
+  function createTodo(description){
+    TodoService.create(description)
+              .then(function(){
+                $scope.description = '';
+                getTodos();
+              });
+  }
+
+  function deleteTodo(id){
+    console.log(id);
+    TodoService.delete(id)
+                .then(function(){
+                  getTodos();
+                });
+  }
+
   }
 })();
